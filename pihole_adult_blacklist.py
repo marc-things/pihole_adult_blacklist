25ac5728a3ec11a88b36c8f413d7da0a2b535e53
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 09:07:59 +0000

    Adding tarfile member sanitization to extractall()

--- pihole_adult_blacklist.py
+++ pihole_adult_blacklist.py
@@ -8,7 +8,29 @@ subprocess.call(['curl', '-o', 'adult.tar.gz', 'http://dsi.ut-capitole.fr/blackl
 
 # extract the contents of the downloaded file
 with tarfile.open('adult.tar.gz') as my_tar:
-    my_tar.extractall()
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(my_tar)
 
 # add domain from each line in the specified file to pihole blacklist
 with open('adult/domains', 'r') as adult_list:
